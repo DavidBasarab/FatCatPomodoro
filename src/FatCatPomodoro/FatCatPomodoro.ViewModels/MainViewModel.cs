00f75dc2d8c5903b05a23da0f4fba0deea9a6ad3
DBasarab <dbasarab617@gmail.com>
2012-02-12 15:12:06 -0500

    Created ViewModels project and added NUGET reference to MVVM Toolkit Light.

---
+++ src/FatCatPomodoro/FatCatPomodoro.ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace FatCatPomodoro.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public string DefaultTestName
+        {
+            get { return "This is from the Main View Model"; }
+        }
+    }
+}
---
+++ src/FatCatPomodoro/FatCatPomodoro.ViewModels/ViewLocator.cs
@@ -0,0 +1,20 @@
+﻿namespace FatCatPomodoro.ViewModels
+{
+    public class ViewLocator
+    {
+        public static MainViewModel MainViewModel { get; private set; }
+        public static bool ViewsCreated { get; set; }
+
+        private static void CreateViewModels()
+        {
+            if (ViewsCreated) return;
+
+            MainViewModel = new MainViewModel();
+        }
+
+        static ViewLocator()
+        {
+            CreateViewModels();
+        }
+    }
+}
